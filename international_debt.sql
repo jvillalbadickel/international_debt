3cde8fd804c7c1f6e89d7685a8aebb8d782b147b
Jorge Villalba Dickel <150525749+jvillalbadickel@users.noreply.github.com>
2023-12-13 08:57:50 -0300

    Update international_debt.sql

--- international_debt.sql
+++ international_debt.sql
@@ -1,3 +1,17 @@
+﻿-- Table: international_debt
+CREATE TABLE international_debt
+(
+  country_name character varying(50),
+  country_code character varying(50),
+  indicator_name text,
+  indicator_code text,
+  debt numeric
+);
+
+-- Copy over data from CSV
+\copy international_debt FROM 'international_debt.csv' DELIMITER ',' CSV HEADER;
+
+
 -- This query retrieves the first 10 rows from the 'international_debt' table
 SELECT *
 FROM international_debt
